b800072baf07bbe1df8dbeb083e022fb88cf2cdf
CodeWithDebu <128091091+CodeWithDebu@users.noreply.github.com>
2023-07-01 21:17:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const RegistrationForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+
     try {
       const response = await axios.post("http://localhost:8000/api/register/", formData);
       console.log(response.data); // Handle the API response as needed
@@ -24,13 +24,63 @@ const RegistrationForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" name="username" placeholder="Username" value={formData.username} onChange={handleChange} />
-      <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} />
-      <input type="password" name="password" placeholder="Password" value={formData.password} onChange={handleChange} />
-      <button type="submit">Register</button>
+    <form onSubmit={handleSubmit} style={styles.container}>
+      <input
+        type="text"
+        name="username"
+        placeholder="Username"
+        value={formData.username}
+        onChange={handleChange}
+        style={styles.input}
+      />
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={formData.email}
+        onChange={handleChange}
+        style={styles.input}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        value={formData.password}
+        onChange={handleChange}
+        style={styles.input}
+      />
+      <button type="submit" style={styles.button}>Register</button>
     </form>
   );
+};
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+  },
+  input: {
+    width: '300px',
+    padding: '10px',
+    margin: '5px',
+    borderRadius: '5px',
+    border: '1px solid gray',
+    fontSize: '16px',
+  },
+  button: {
+    width: '300px',
+    padding: '10px',
+    margin: '5px',
+    borderRadius: '5px',
+    border: 'none',
+    background: 'blue',
+    color: 'white',
+    fontSize: '16px',
+    cursor: 'pointer',
+  },
 };
 
 export default RegistrationForm;
